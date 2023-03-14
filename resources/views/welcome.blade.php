d2c9f72f8ed93eca3f6386056f1c5f1a6ee1e21c
Zineb Atlassi <atlassizineb@gmail.com>
2023-03-14 16:49:10 +0100

    Inizializzazione file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,30 +4,40 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 + Bootstrap Template</title>
+        <title>Laravel DC Comics</title>
 
         {{-- Includiamo gli assets con la direttiva @vite --}}
         @vite('resources/js/app.js')
     </head>
     <body>
 
+        <header>
+            HEADER
+        </header>
+
         <main>
             <div class="container">
+
+                <h1 class="text-center">
+                    Laravel DC Comics
+                </h1>
+
                 <div class="row">
                     <div class="col text-center">
 
                         <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
-
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
-                            </div>
+                            
                         </div>
 
                     </div>
                 </div>
+
             </div>
         </main>
+
+        <footer>
+            FOOTER
+        </footer>
 
     </body>
 </html>
